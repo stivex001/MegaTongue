1048d7d4622d07b7bb3a96c9f184cb81d72f9932
stivex001 <stephenadeyemo@gmail.com>
2023-10-02 03:00:39 +0100

    feat: implemented the navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import CustomButton from "../utils/CustomButton";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
+  const flexBetween = "flex items-center justify-between";
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="my-5">
+      <div className={`${flexBetween}`}>
+        <div className="flex items-center gap-2.5">
+          <img src={Logo} alt="megatongue" className="w-12 h-12" />
+          <span className="text-purple-20 text-2xl font-poppins font-semibold">
+            MegaTongue
+          </span>
+        </div>
+        <div className={`flex gap-10 text-xl font-normal`}>
+          <Link to="#">Pricing</Link>
+          <Link to="#">API Documentation</Link>
+          <Link to="#">Pricing</Link>
+        </div>
+        <div className="flex">
+          <CustomButton className="">Signup</CustomButton>
+          <Bars3Icon />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
