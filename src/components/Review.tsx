2456f2c305ecdb96739d48cd4fd1a16b2eaa7948
stivex001 <stephenadeyemo@gmail.com>
2023-10-05 02:57:20 +0100

    added scroolable card

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,30 +1,59 @@
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 import Card from "./Card";
-
-// type Props = {};
+import React, { useRef } from "react";
 
 const Review = () => {
   const flexBetween = "flex items-center justify-between";
+  const cardContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollLeft = () => {
+    cardContainerRef.current?.scrollBy({
+      left: -200, // Adjust the scroll distance as needed
+      behavior: "smooth",
+    });
+  };
+
+  const scrollRight = () => {
+    cardContainerRef.current?.scrollBy({
+      left: 200, // Adjust the scroll distance as needed
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="w-full h-full">
       <div className="w-5/6 mx-auto my-6 ">
         <div className={`${flexBetween}`}>
           <div>
             <span className="text-purple-20 text-sm font-medium">
-              What customers says
+              What customers say
             </span>
             <p className="text-dark-20 text-4xl font-bold">Recent Reviews</p>
           </div>
           <div className="flex items-center gap-4">
-            <div className="flex items-center justify-center py-3 px-6 bg-[rgba(18, 17, 39, 0.04)] rounded-[100px]">
+            <div
+              className="flex items-center justify-center py-3 px-6 bg-[rgba(18, 17, 39, 0.04)] rounded-[100px] cursor-pointer"
+              onClick={scrollLeft}
+            >
               <BsArrowLeft size={24} />
             </div>
-            <div className="flex items-center justify-center py-3 px-6 bg-purple-20 rounded-[100px] text-white">
+            <div
+              className="flex items-center justify-center py-3 px-6 bg-purple-20 rounded-[100px] text-white cursor-pointer"
+              onClick={scrollRight}
+            >
               <BsArrowRight size={24} />
             </div>
           </div>
         </div>
-        <div className="flex items-center gap-6 mt-[41px]">
+        <div
+          className="flex items-center gap-6 mt-[41px] overflow-hidden"
+          style={{
+            overflowX: "scroll",
+            scrollbarWidth: "none", // Hide the scrollbar for Firefox
+            WebkitOverflowScrolling: "touch",
+          }}
+          ref={cardContainerRef}
+        >
           <Card />
           <Card />
           <Card />
