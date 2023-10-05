d083abb72123c050d6f9622178c8bef131a995db
stivex001 <stephenadeyemo@gmail.com>
2023-10-05 02:09:56 +0100

    added more contents to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,10 @@
 // type Props = {};
 
+import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import CustomButton from "../utils/CustomButton";
+
 const Footer = () => {
+    const flexBetween = "flex items-center justify-between";
   return (
     <footer className="py-16 mt-40">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
@@ -10,9 +14,6 @@ const Footer = () => {
           <p className="my-5 font-normal text-sm text-dark-20">Analytics</p>
           <p className="my-5 font-normal text-sm text-dark-20">Commerce</p>
           <p className="my-5 font-normal text-sm text-dark-20">Insights</p>
-          <p className="font-normal text-sm text-gray-400">
-            &copy; WORKOUT All Rights Reserved.
-          </p>
         </div>
         <div className="mt-16 basis-1/2 md:mt-0">
           <h4 className="  text-[#e1e1e4] tex-xl font-bold">Support</h4>
@@ -34,6 +35,37 @@ const Footer = () => {
           <p className="my-5 font-normal text-sm text-dark-20">Claim</p>
           <p className="my-5 font-normal text-sm text-dark-20">Privacy</p>
           <p className="my-5 font-normal text-sm text-dark-20">Terms</p>
+        </div>
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <h4 className="  text-[#e1e1e4] tex-xl font-bold">
+            Subscribe to our newsletter
+          </h4>
+          <p className="my-5 font-normal text-sm text-dark-20 max-w-xs">
+            The latest news, articles, and resources, sent to your inbox weekly
+          </p>
+          <div className="flex items-center gap-3">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="py-1.5 px-3"
+            />
+            <CustomButton className="w-fit py-2 px-6 border-2 outline-[rgba(18, 17, 39, 0.12)]">
+              Subscribe
+            </CustomButton>
+          </div>
+        </div>
+      </div>
+      <div className={`${flexBetween} mx-auto w-5/6 gap-16 md:flex`}>
+        <div>
+          <p className="font-normal text-sm text-gray-400">
+            &copy; 2023 megatongue. All rights reserved
+          </p>
+        </div>
+        <div className="flex items-center gap-10 text-gray-400">
+          <BsFacebook />
+          <BsTwitter />
+          <BsInstagram />
+          <BsGithub />
         </div>
       </div>
     </footer>
